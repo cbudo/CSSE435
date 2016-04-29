4ae9cb52aedaf0dba0ae4eac66641eb2602ae573
cbudo <budocf@rose-hulman.edu>
2016-04-29 12:09:34 -0400

    added in class activity and start of lab 6

---
+++ Android/In Class/GolfBallDelivery/README.md
@@ -0,0 +1,4 @@
+GolfBallDelivery
+================
+
+Code to help jump start your Golf Ball Delivery efforts.
---
+++ Android/Lab6/GolfBallSorting/app/src/main/java/edu/rosehulman/budocf/golfballsorting/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.rosehulman.budocf.golfballsorting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.rosehulman.budocf.golfballsorting.me435.RobotActivity;
+
+public class MainActivity extends RobotActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
