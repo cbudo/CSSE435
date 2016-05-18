e14ad830e8ae80a3147d439381405cdee275c981
asleeponduty <lukedrong@gmail.com>
2016-05-17 23:10:50 -0400

    Maybe fixed a bug in inverse line counting

--- Android/FinalProject/IntegratedImageRec/app/src/main/java/edu/rosehulman/drongla/integratedimagerec/GolfBallDeliveryActivity.java
+++ Android/FinalProject/IntegratedImageRec/app/src/main/java/edu/rosehulman/drongla/integratedimagerec/GolfBallDeliveryActivity.java
@@ -1441,8 +1441,8 @@ public class GolfBallDeliveryActivity extends ImageRecActivity {
             return 0;
         }
         int lines = 1;
-        int pos = 0;
-        while ((pos = str.lastIndexOf("\n", pos) + 1) != 0) {
+        int pos = str.lastIndexOf("\n");
+        while ((pos = str.lastIndexOf("\n", pos) - 1) != 0) {
             lines++;
             if (lines > (X)){
                 return pos / 2;
